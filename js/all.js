3a5b92c7770faf63b0d5bc171a3ce63535a85881
“Harry” <boom0525300@gmail.com>
2023-03-14 17:58:44 +0800

    creat subscribe.html & finish 50%

---
+++ js/all.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function () {
+
+    // box shadow
+    $('.select-btn').on('click', function () {
+        $('.select-btn').removeClass('btn-active-shadow');
+        $(this).addClass('btn-active-shadow');
+    });
+
+});
