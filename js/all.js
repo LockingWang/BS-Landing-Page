120089b6780e2a38c125576da087a5f4adbd0ee5
“Harry” <boom0525300@gmail.com>
2023-03-15 10:33:53 +0800

    finish subscribe.html

--- js/all.js
+++ js/all.js
@@ -1,10 +1,40 @@
 
 $(document).ready(function () {
 
-    // box shadow
+    // box shadow & text-color
     $('.select-btn').on('click', function () {
+      
         $('.select-btn').removeClass('btn-active-shadow');
         $(this).addClass('btn-active-shadow');
+
+        $('.select-btn').each(function(index, element){
+          if ($(element).attr('class').includes('btn-active-shadow')) {
+            $(element).children('p').addClass('text-white');
+          }else {
+            $(element).children('p').removeClass('text-white');
+          }
+        })
     });
 
 });
+
+// form validation
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
